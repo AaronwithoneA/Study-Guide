a2cdd491801d6f53cc818cc55096da2aee631954
Aron Cutler <acutler84@gmail.com>
2017-07-10 19:41:53 -0700

    Complete augmentElements recursive function

--- recursion-prompts-master/src/recursion.js
+++ recursion-prompts-master/src/recursion.js
@@ -402,11 +402,33 @@ var nestedEvenSum = function(obj) {
 // 30. Flatten an array containing nested arrays.
 // flatten([1,[2],[3,[[4]]],5]); // [1,2,3,4,5]
 var flatten = function(array) {
+
+  let flattened = [];
+
+  for(let i = 0; i < array.length; i++) {
+    let current = array[i];
+
+    if (Array.isArray(current)) {
+      flattened = flattened.concat(flatten(current));
+    } else {
+      flattened.push(current);
+    }
+  }
+  return flattened;
 };
 
 // 31. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {p:1, o:2, t:2, a:1}
 var letterTally = function(str, obj) {
+  if (str.length < 1) {
+    let key = str[0];
+    return {key: 1};
+  }
+
+  if (obj[str[0]]) {
+
+  }
+
 };
 
 // 32. Eliminate consecutive duplicates in a list. If the list contains repeated
@@ -415,12 +437,32 @@ var letterTally = function(str, obj) {
 // compress([1,2,2,3,4,4,5,5,5]) // [1,2,3,4,5]
 // compress([1,2,2,3,4,4,2,5,5,5,4,4]) // [1,2,3,4,2,5,4]
 var compress = function(list) {
-};
+  let compressed = []
+  if (list.length <= 1) {
+    return [list[0]];
+  }
+  let next = compress(list.slice(1));
+  if (next.indexOf(list[0]) === -1) {
+    return compressed.concat(next);
+  }
+
+}
+
+
+
+
 
 // 33. Augument every element in a list with a new value where each element is an array
 // itself.
 // augmentElements([[],[3],[7]], 5); // [[5],[3,5],[7,5]]
 var augmentElements = function(array, aug) {
+  if (array.length === 0) {
+    return [];
+  }
+
+  let augmentedElement = array[0];
+  augmentedElement.push(aug)
+  return [augmentedElement].concat(augmentElements(array.slice(1), aug))
 };
 
 // 34. Reduce a series of zeroes to a single 0.
