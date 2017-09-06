f684057e395bec48b4d9152db2e336b8a5e7504a
Aron Cutler <acutler84@gmail.com>
2017-09-06 03:12:14 -0700

    Complete binary search

--- Algorithm_Practice.js
+++ Algorithm_Practice.js
@@ -16,3 +16,20 @@ const merge = (left, right) => {
   }
   return merged;
 };
+
+const bSearch = (arr, target) => {
+  if (arr.length < 1) {
+    return -1;
+  }
+  const pivotIdx = arr.length/2;
+  const pivot = arr[pivotIdx];
+  if (target < pivot) {
+    let left = arr.slice(0, pivotIdx);
+    return bSearch(left);
+  }
+  if (target < pivot) {
+    let right = arr.slice(pivotIdx + 1);
+    let sub = bSearch(right);
+    sub === -1 ? -1 : sub + pivot + 1;
+  }
+};
