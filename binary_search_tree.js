0687c7c67a1148e05810cca7cc63bba97138e0ca
Aron Cutler <acutler84@gmail.com>
2017-09-11 03:12:14 -0700

    Write binarysearchtree add method

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -7,3 +7,51 @@ function Node(data) {
   this.left = null;
   this.righ = null;
 }
+
+BinarySearchTree.prototype.add = function(data) {
+  let node = new Node(data);
+
+  if(!this.root) {
+    this.root = node;
+  } else {
+    let current = this.root;
+    while(current){
+      if(node.data < current.data) {
+        if(!current.left) {
+          current.left = node;
+          break;
+        }
+        current = current.left;
+      } else  {
+        if(!current.right) {
+          current.right = node;
+        }
+        current = current.right;
+      }
+    }
+  }
+};
+
+
+
+
+
+
+
+
+
+BinarySearchTree.prototype.add = function(data) {
+
+}
+BinarySearchTree.prototype.add = function(data) {
+
+}
+BinarySearchTree.prototype.add = function(data) {
+
+}
+BinarySearchTree.prototype.add = function(data) {
+
+}
+BinarySearchTree.prototype.add = function(data) {
+
+}
