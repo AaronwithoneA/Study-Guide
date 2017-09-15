bdb4842d974a0adc8877489bb918f45a7d2a007d
Aron Cutler <acutler84@gmail.com>
2017-09-15 14:39:33 -0700

    Write doublylinkedlist add method

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -9,3 +9,18 @@ function Node(data) {
   this.next = null;
   this.previous = null;
 }
+
+DoublyLinkedList.prototype.add = function(data) {
+  let node = new Node(data);
+
+  if(!this.head) {
+    this.head = node;
+    this.tail = node;
+  } else {
+    node.previous = this.tail;
+    this.tail.next = node;
+    this.tail = node;
+  }
+
+  this.length++;
+};
