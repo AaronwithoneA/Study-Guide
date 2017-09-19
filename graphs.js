df01a069c92c28d4812ec35d9d68d0a796187ce0
Aron Cutler <acutler84@gmail.com>
2017-09-19 10:52:49 -0700

    Write graph addEdge method

--- graphs.js
+++ graphs.js
@@ -7,3 +7,16 @@ Graph.prototype.addVertex = function (vertex) {
   this.vertices.push(vertex);
   this.edges[vertex] = [];
 };
+
+Graph.prototype.addEdge = function (vertex1, vertex2) {
+  if(!vertex1 && !vertex2) {
+    return 'Neither vertex exists';
+  } else if(!vertex1) {
+    return 'Vertex1 does not exist';
+  } else if(!vertex2) {
+    return 'Vertex2 does not exist';
+  } else {
+    this.edges[vertex1].push(vertex2);
+    this.edge(vertex2).push(vertex1);
+  }
+};
