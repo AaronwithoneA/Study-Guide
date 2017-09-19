137a859eb04caa641b594d238e45592b6353f849
Aron Cutler <acutler84@gmail.com>
2017-09-19 12:14:53 -0700

    Write topologicalSort method

--- graphs.js
+++ graphs.js
@@ -46,3 +46,37 @@ Graph.prototype.traverseDFS_ = function(vertex, visited, fn) {
     }
   }
 };
+
+Graph.prototype.topologicalSort = function() {
+  let sorted = [];
+  let queue = [];
+  let edges = this.edges;
+
+  for(let i = 0; i < this.vertices.length; i++) {
+    let vertex = this.vertices[i];
+    if(vertex.inEdges.length === 0) {
+      queue.push(vertex);
+    }
+  }
+
+  while(queue.length) {
+    let current = queue.pop();
+    sorted.push(current);
+    let destinations = current.outEdges;
+
+    for(let i = 0; i < destinations.length; i++) {
+      let destination = destinations[i];
+      if(destination.inEdges.length === 0) {
+        queue.push(destination);
+      }
+      let index = destination.inEdges.indexOf(current);
+      destination.inEdges.splice(index, 1);
+    }
+
+    current.outEdges = [];
+    let vertexIndex = this.vertices.indexOf(current);
+    this.vertices.splice(vertexIndex, 1);
+  }
+
+  return sorted;
+};
