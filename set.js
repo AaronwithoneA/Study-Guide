40f3f08d7a3fd1a0949a215ba823ac596240942e
Aron Cutler <acutler84@gmail.com>
2017-09-20 03:12:14 -0700

    Write set add method

--- set.js
+++ set.js
@@ -2,3 +2,10 @@ function Set() {
   this.values = [];
   this.numberOfValues = 0;
 }
+
+Set.prototype.add = function(value) {
+  if(!~this.values.indexOf(value)) {
+    this.values.push(value);
+    this.numberOfValues++;
+  }
+};
