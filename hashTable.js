daaad4f4deedc75fefd9506dae820b8814a14ed5
Aron Cutler <acutler84@gmail.com>
2017-09-25 03:12:14 -0700

    Write hashTable add method

--- hashTable.js
+++ hashTable.js
@@ -3,3 +3,14 @@ function HashTable(size) {
   this.numberOfValues = 0;
   this.size = size;
 }
+
+HashTable.prototype.add = function(key, value) {
+  var hash = this.calculateHash(key);
+  if(!this.values.hasOwnProperty(hash)) {
+    this.values[hash] = {};
+  }
+  if(!this.values[hash].hasOwnProperty(key)) {
+    this.numberOfValues++;
+  }
+  this.values[hash][key] = value;
+};
