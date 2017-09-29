16aa9179d17a96412fd44d32d9665912a4ec8559
AaronwithoneA <acutler84@gmail.com>
2017-09-29 12:10:22 -0700

    Write unique anagram function

--- Algorithm_Practice.js
+++ Algorithm_Practice.js
@@ -114,3 +114,19 @@ function safeSpeedChange(speeds) {
 
   return true;
 }
+
+function isUniqueAnagram(word1, word2) {
+  if (word1.length !== word2.length) {
+    return false;
+  }
+
+  for (var i = 0; i < word1.length; i += 1) {
+    var char = word1[i];
+
+    if (word2.indexOf(char) === -1) {
+      return false;
+    }
+  }
+
+  return true;
+}
