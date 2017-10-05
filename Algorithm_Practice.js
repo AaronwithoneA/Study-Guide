0d0ea280c9bd307885f50cbb4df9f79a85df32a3
AaronwithoneA <acutler84@gmail.com>
2017-10-05 12:10:22 -0700

    Write iterativefib function

--- Algorithm_Practice.js
+++ Algorithm_Practice.js
@@ -232,3 +232,23 @@ function uncompressString(str) {
 
   return newStr;
 }
+
+function fibonacci(n) {
+  if (n === 0) {
+    return [];
+  } else if (n === 1) {
+    return [1];
+  }
+
+  var seq = [1, 1];
+
+  for (var i = 2; i < n; i += 1) {
+    var last = seq[seq.length - 1];
+    var secondLast = seq[seq.length - 2];
+    var next = last + secondLast;
+
+    seq.push(next);
+  }
+
+  return seq;
+}
